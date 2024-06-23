48efc4c18099fd2bb5b2545923babcd61a8a6c73
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-06-23 16:02:57 -0700

    added redux logic for actions with new container,variable,relations,and nodes

--- dynamic-variable-diagrams/src/store/reducers.js
+++ dynamic-variable-diagrams/src/store/reducers.js
@@ -1,5 +1,5 @@
 // src/store/reducers.js
-import { ADD_CONTAINER, UPDATE_CONTAINER } from './actions';
+import { ADD_CONTAINER, UPDATE_CONTAINER, DELETE_CONTAINER, ADD_RELATION, UPDATE_RELATION, DELETE_RELATION, ADD_VARIABLE, UPDATE_VARIABLE, DELETE_VARIABLE, ADD_NODE_TO_CONTAINER, DELETE_NODE_FROM_CONTAINER } from './actions';
 
 const initialState = {
   containers: [
@@ -14,16 +14,101 @@ const diagramReducer = (state = initialState, action) => {
     case ADD_CONTAINER:
       return {
         ...state,
-        containers: [...state.containers, action.payload],
+        containers: {
+          ...state.containers,
+          [action.payload.containerId]: { nodes: [], variables: {}, relations: {} },
+        },
       };
-    case UPDATE_CONTAINER:
+    case DELETE_CONTAINER:
+      const { [action.payload.containerId]: _, ...restContainers } = state.containers;
       return {
         ...state,
-        containers: state.containers.map(container =>
-          container.id === action.payload.id ? action.payload : container
-        ),
+        containers: restContainers,
       };
-      console.log(state);
+    case ADD_NODE_TO_CONTAINER:
+      return {
+        ...state,
+        containers: {
+          ...state.containers,
+          [action.payload.containerId]: {
+            ...state.containers[action.payload.containerId],
+            nodes: [...state.containers[action.payload.containerId].nodes, action.payload.nodeId],
+          },
+        },
+      };
+    case DELETE_NODE_FROM_CONTAINER:
+      return {
+        ...state,
+        containers: {
+          ...state.containers,
+          [action.payload.containerId]: {
+            ...state.containers[action.payload.containerId],
+            nodes: state.containers[action.payload.containerId].nodes.filter(
+              (nodeId) => nodeId !== action.payload.nodeId
+            ),
+          },
+        },
+      };
+    case ADD_VARIABLE:
+    case UPDATE_VARIABLE:
+      return {
+        ...state,
+        containers: {
+          ...state.containers,
+          [action.payload.containerId]: {
+            ...state.containers[action.payload.containerId],
+            variables: {
+              ...state.containers[action.payload.containerId].variables,
+              [action.payload.name]: action.payload.value,
+            },
+          },
+        },
+      };
+    case DELETE_VARIABLE:
+      const {
+        [action.payload.name]: __,
+        ...restVariables
+      } = state.containers[action.payload.containerId].variables;
+      return {
+        ...state,
+        containers: {
+          ...state.containers,
+          [action.payload.containerId]: {
+            ...state.containers[action.payload.containerId],
+            variables: restVariables,
+          },
+        },
+      };
+    case ADD_RELATION:
+    case UPDATE_RELATION:
+      return {
+        ...state,
+        containers: {
+          ...state.containers,
+          [action.payload.containerId]: {
+            ...state.containers[action.payload.containerId],
+            relations: {
+              ...state.containers[action.payload.containerId].relations,
+              [action.payload.name]: action.payload.formula,
+            },
+          },
+        },
+      };
+    case DELETE_RELATION:
+      const {
+        [action.payload.name]: ___,
+        ...restRelations
+      } = state.containers[action.payload.containerId].relations;
+      return {
+        ...state,
+        containers: {
+          ...state.containers,
+          [action.payload.containerId]: {
+            ...state.containers[action.payload.containerId],
+            relations: restRelations,
+          },
+        },
+      };
     default:
       return state;
   }
