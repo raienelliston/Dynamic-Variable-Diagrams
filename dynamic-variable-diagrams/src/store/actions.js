131c1b6ac846922213376d8fdd0ef86b6479f4ab
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-06-23 17:52:58 -0700

    fixed relations and variable logic from container.map issue

--- dynamic-variable-diagrams/src/store/actions.js
+++ dynamic-variable-diagrams/src/store/actions.js
@@ -1,6 +1,15 @@
 // src/store/actions.js
 export const ADD_CONTAINER = 'ADD_CONTAINER';
 export const UPDATE_CONTAINER = 'UPDATE_CONTAINER';
+export const DELETE_CONTAINER = 'DELETE_CONTAINER';
+export const ADD_RELATION = 'ADD_RELATION';
+export const UPDATE_RELATION = 'UPDATE_RELATION';
+export const DELETE_RELATION = 'DELETE_RELATION';
+export const ADD_VARIABLE = 'ADD_VARIABLE';
+export const UPDATE_VARIABLE = 'UPDATE_VARIABLE';
+export const DELETE_VARIABLE = 'DELETE_VARIABLE';
+export const ADD_NODE_TO_CONTAINER = 'ADD_NODE_TO_CONTAINER';
+export const DELETE_NODE_FROM_CONTAINER = 'DELETE_NODE_FROM_CONTAINER';
 
 export const addContainer = (container) => ({
   type: ADD_CONTAINER,
@@ -10,4 +19,49 @@ export const addContainer = (container) => ({
 export const updateContainer = (container) => ({
   type: UPDATE_CONTAINER,
   payload: container,
+});
+
+export const deleteContainer = (container) => ({
+  type: DELETE_CONTAINER,
+  payload: container,
+});
+
+export const addRelation = (relation) => ({
+  type: ADD_RELATION,
+  payload: relation,
+});
+
+export const updateRelation = (relation) => ({
+  type: UPDATE_RELATION,
+  payload: relation,
+});
+
+export const deleteRelation = (relation) => ({
+  type: DELETE_RELATION,
+  payload: relation,
+});
+
+export const addVariable = (variable) => ({
+  type: ADD_VARIABLE,
+  payload: variable,
+});
+
+export const updateVariable = (variable) => ({
+  type: UPDATE_VARIABLE,
+  payload: variable,
+});
+
+export const deleteVariable = (variable) => ({
+  type: DELETE_VARIABLE,
+  payload: variable,
+});
+
+export const addNodeToContainer = (node) => ({
+  type: ADD_NODE_TO_CONTAINER,
+  payload: node,
+});
+
+export const deleteNodeFromContainer = (node) => ({
+  type: DELETE_NODE_FROM_CONTAINER,
+  payload: node,
 });
--- dynamic-variable-diagrams/src/store/reducers.js
+++ dynamic-variable-diagrams/src/store/reducers.js
@@ -1,5 +1,5 @@
 // src/store/reducers.js
-import { ADD_CONTAINER, UPDATE_CONTAINER } from './actions';
+import { ADD_CONTAINER, UPDATE_CONTAINER, DELETE_CONTAINER, ADD_RELATION, UPDATE_RELATION, DELETE_RELATION, ADD_VARIABLE, UPDATE_VARIABLE, DELETE_VARIABLE, ADD_NODE_TO_CONTAINER, DELETE_NODE_FROM_CONTAINER } from './actions';
 
 const initialState = {
   containers: [
@@ -7,6 +7,8 @@ const initialState = {
     { id: 2, name: 'Container 2', text: "", x: 200, y: 100, changableVars: [], dependentVars: [] },
     { id: 3, name: 'Container 3', text: "", x: 400, y: 200, changableVars: [], dependentVars: [] },
 ],
+  relations: [],
+  variables: [],
 };
 
 const diagramReducer = (state = initialState, action) => {
@@ -23,7 +25,49 @@ const diagramReducer = (state = initialState, action) => {
           container.id === action.payload.id ? action.payload : container
         ),
       };
-      console.log(state);
+    case DELETE_CONTAINER:
+      return {
+        ...state,
+        containers: state.containers.filter(container => container.id !== action.payload.id),
+      };
+    case ADD_RELATION:
+      return {
+        ...state,
+        relations: [...state.relations, action.payload],
+      };
+    case UPDATE_RELATION:
+      return {
+        ...state,
+        relations: state.relations.map(relation =>
+          relation.id === action.payload.id ? action.payload : relation
+        ),
+      };
+    case DELETE_RELATION:
+      return {
+        ...state,
+        relations: state.relations.filter(relation => relation.id !== action.payload.id),
+      };
+    case ADD_VARIABLE:
+      return {
+        ...state,
+        variables: [...state.variables, action.payload],
+      };
+    case UPDATE_VARIABLE:
+      return {
+        ...state,
+        variables: state.variables.map(variable =>
+          variable.id === action.payload.id ? action.payload : variable
+        ),
+      };
+    case DELETE_VARIABLE:
+      return {
+        ...state,
+        variables: state.variables.filter(variable => variable.id !== action.payload.id),
+      };
+    case ADD_NODE_TO_CONTAINER:
+      return state;
+    case DELETE_NODE_FROM_CONTAINER:
+      return state;
     default:
       return state;
   }
