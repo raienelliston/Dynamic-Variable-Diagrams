30011927e67311e83ccd9ddf61dc98a881e94537
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-06-23 17:37:07 -0700

    Revert "added redux logic for actions with new container,variable,relations,and nodes"
    This reverts commit 48efc4c18099fd2bb5b2545923babcd61a8a6c73.

--- dynamic-variable-diagrams/src/store/actions.js
+++ dynamic-variable-diagrams/src/store/actions.js
@@ -1,67 +1,13 @@
 // src/store/actions.js
 export const ADD_CONTAINER = 'ADD_CONTAINER';
 export const UPDATE_CONTAINER = 'UPDATE_CONTAINER';
-export const DELETE_CONTAINER = 'DELETE_CONTAINER';
-export const ADD_RELATION = 'ADD_RELATION';
-export const UPDATE_RELATION = 'UPDATE_RELATION';
-export const DELETE_RELATION = 'DELETE_RELATION';
-export const ADD_VARIABLE = 'ADD_VARIABLE';
-export const UPDATE_VARIABLE = 'UPDATE_VARIABLE';
-export const DELETE_VARIABLE = 'DELETE_VARIABLE';
-export const ADD_NODE_TO_CONTAINER = 'ADD_NODE_TO_CONTAINER';
-export const DELETE_NODE_FROM_CONTAINER = 'DELETE_NODE_FROM_CONTAINER';
 
-export const addContainer = (containerId, name, text) => ({
+export const addContainer = (container) => ({
   type: ADD_CONTAINER,
-  payload: { containerId, name, text},
+  payload: container,
 });
 
-export const updateContainer = (containerId, name, text) => ({
+export const updateContainer = (container) => ({
   type: UPDATE_CONTAINER,
-  payload: { containerId, name, text},
-});
-
-export const deleteContainer = (containerId) => ({
-  type: DELETE_CONTAINER,
-  payload: { containerId },
-});
-
-export const addRelation = (containerId, name, formula) => ({
-  type: ADD_RELATION,
-  payload: { containerId, name, formula },
-});
-
-export const updateRelation = (containerId, name, formula) => ({
-  type: UPDATE_RELATION,
-  payload: { containerId, name, formula },
-});
-
-export const deleteRelation = (containerId, name) => ({
-  type: DELETE_RELATION,
-  payload: { containerId, name },
-});
-
-export const addVariable = (containerId, name, value) => ({
-  type: ADD_VARIABLE,
-  payload: { containerId, name, value },
-});
-
-export const updateVariable = (containerId, name, value) => ({
-  type: UPDATE_VARIABLE,
-  payload: { containerId, name, value },
-});
-
-export const deleteVariable = (containerId, name) => ({
-  type: DELETE_VARIABLE,
-  payload: { containerId, name },
-});
-
-export const addNodeToContainer = (containerId, nodeId) => ({
-  type: ADD_NODE_TO_CONTAINER,
-  payload: { containerId, nodeId },
-});
-
-export const deleteNodeFromContainer = (containerId, nodeId) => ({
-  type: DELETE_NODE_FROM_CONTAINER,
-  payload: { containerId, nodeId },
+  payload: container,
 });
--- dynamic-variable-diagrams/src/store/reducers.js
+++ dynamic-variable-diagrams/src/store/reducers.js
@@ -1,5 +1,5 @@
 // src/store/reducers.js
-import { ADD_CONTAINER, UPDATE_CONTAINER, DELETE_CONTAINER, ADD_RELATION, UPDATE_RELATION, DELETE_RELATION, ADD_VARIABLE, UPDATE_VARIABLE, DELETE_VARIABLE, ADD_NODE_TO_CONTAINER, DELETE_NODE_FROM_CONTAINER } from './actions';
+import { ADD_CONTAINER, UPDATE_CONTAINER } from './actions';
 
 const initialState = {
   containers: [
@@ -14,101 +14,16 @@ const diagramReducer = (state = initialState, action) => {
     case ADD_CONTAINER:
       return {
         ...state,
-        containers: {
-          ...state.containers,
-          [action.payload.containerId]: { nodes: [], variables: {}, relations: {} },
-        },
+        containers: [...state.containers, action.payload],
       };
-    case DELETE_CONTAINER:
-      const { [action.payload.containerId]: _, ...restContainers } = state.containers;
+    case UPDATE_CONTAINER:
       return {
         ...state,
-        containers: restContainers,
+        containers: state.containers.map(container =>
+          container.id === action.payload.id ? action.payload : container
+        ),
       };
-    case ADD_NODE_TO_CONTAINER:
-      return {
-        ...state,
-        containers: {
-          ...state.containers,
-          [action.payload.containerId]: {
-            ...state.containers[action.payload.containerId],
-            nodes: [...state.containers[action.payload.containerId].nodes, action.payload.nodeId],
-          },
-        },
-      };
-    case DELETE_NODE_FROM_CONTAINER:
-      return {
-        ...state,
-        containers: {
-          ...state.containers,
-          [action.payload.containerId]: {
-            ...state.containers[action.payload.containerId],
-            nodes: state.containers[action.payload.containerId].nodes.filter(
-              (nodeId) => nodeId !== action.payload.nodeId
-            ),
-          },
-        },
-      };
-    case ADD_VARIABLE:
-    case UPDATE_VARIABLE:
-      return {
-        ...state,
-        containers: {
-          ...state.containers,
-          [action.payload.containerId]: {
-            ...state.containers[action.payload.containerId],
-            variables: {
-              ...state.containers[action.payload.containerId].variables,
-              [action.payload.name]: action.payload.value,
-            },
-          },
-        },
-      };
-    case DELETE_VARIABLE:
-      const {
-        [action.payload.name]: __,
-        ...restVariables
-      } = state.containers[action.payload.containerId].variables;
-      return {
-        ...state,
-        containers: {
-          ...state.containers,
-          [action.payload.containerId]: {
-            ...state.containers[action.payload.containerId],
-            variables: restVariables,
-          },
-        },
-      };
-    case ADD_RELATION:
-    case UPDATE_RELATION:
-      return {
-        ...state,
-        containers: {
-          ...state.containers,
-          [action.payload.containerId]: {
-            ...state.containers[action.payload.containerId],
-            relations: {
-              ...state.containers[action.payload.containerId].relations,
-              [action.payload.name]: action.payload.formula,
-            },
-          },
-        },
-      };
-    case DELETE_RELATION:
-      const {
-        [action.payload.name]: ___,
-        ...restRelations
-      } = state.containers[action.payload.containerId].relations;
-      return {
-        ...state,
-        containers: {
-          ...state.containers,
-          [action.payload.containerId]: {
-            ...state.containers[action.payload.containerId],
-            relations: restRelations,
-          },
-        },
-      };
+      console.log(state);
     default:
       return state;
   }
